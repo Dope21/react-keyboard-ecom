bb0ba2c7d24d89a57db471315cc533f8210dd5e9
ten14301 <ten077710@gmail.com>
2023-04-11 19:59:30 +0700

    add

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -2,21 +2,39 @@ import { useState } from 'react';
 import { BsBag } from 'react-icons/bs';
 import { FiX } from 'react-icons/fi';
 
+interface ProductInfo {
+  id: string;
+  image: string;
+  description: string;
+  price: number;
+}
+
+interface CartItem extends ProductInfo {
+  quantity: number;
+}
+
 const CartDrawer = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [quantity, setQuantity] = useState(1);
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
   const toggleCart = () => {
     setIsOpen(!isOpen);
   };
 
-  const handleAdd = () => {
-    setQuantity(quantity + 1);
-  };
-
-  const handleSubtract = () => {
-    if (quantity > 1) {
-      setQuantity(quantity - 1);
+  const handleAddToCart = (product: ProductInfo) => {
+    // Check if the product is already in the cart
+    const existingCartItemIndex = cartItems.findIndex((item) => item.id === product.id);
+    if (existingCartItemIndex >= 0) {
+      // If the product is already in the cart, update its quantity
+      const existingCartItem = cartItems[existingCartItemIndex];
+      const updatedCartItem = { ...existingCartItem, quantity: existingCartItem.quantity + 1 };
+      const updatedCartItems = [...cartItems];
+      updatedCartItems[existingCartItemIndex] = updatedCartItem;
+      setCartItems(updatedCartItems);
+    } else {
+      // If the product is not yet in the cart, add it
+      const newCartItem = { ...product, quantity: 1 };
+      setCartItems([...cartItems, newCartItem]);
     }
   };
 
@@ -29,7 +47,6 @@ const CartDrawer = () => {
         <div className="fixed inset-0 z-50 overflow-hidden">
           <div className="absolute inset-0 overflow-hidden">
             <div className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true" />
-            
             <section className="absolute inset-y-0 right-0 pl-10 max-w-full flex">
               <div className="relative w-screen max-w-md">
                 <div className="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-scroll">
@@ -45,38 +62,37 @@ const CartDrawer = () => {
                     </div>
                   </div>
                   <div className="px-4 sm:px-6 mt-6">
-                    <div className="flex items-center justify-between">
-                      <div className="flex items-center">
-                        <img src="https://i.ibb.co/N9jDpvF/2.png" className="w-8 h-8 mr-2" />
-                        <p className="text-lg font-medium text-gray-900">Dummy Item</p>
-                      </div>
-                      <div className="flex items-center">
-                        <button className="text-gray-500 hover:text-gray-700 focus:outline-none" onClick={handleSubtract}>
-                          -
-                        </button>
-                        <span className="mx-2 text-gray-700">{quantity}</span>
-                        <button className="text-gray-500 hover:text-gray-700 focus:outline-none" onClick={handleAdd}>
-                          +
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="px-4 sm:px-6 mt-6">
-                    <p className="text-lg font-medium text-gray-900">Total: $0.00</p>
-                  </div>
-                  <div className="px-4 sm:px-6 mt-6">
-                    <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
-                      Checkout
-                    </button>
-                  </div>
-                </div>
+                   
+                  {cartItems.length === 0 ? (
+                  <p className="text-center text-gray-500">Your cart is empty.</p>
+                ) : (
+                  <ul role="list" className="divide-y divide-gray-200">
+                    {cartItems.map((item) => (
+                      <li key={item.id} className="py-4 flex">
+                        <img className="h-10 w-10 rounded-full" src={item.image} alt={item.description} />
+                        <div className="ml-3 flex-1">
+                          <div className="flex items-center justify-between">
+                            <h3 className="text-sm font-medium text-gray-900">{item.description}</h3>
+                            <p className="text-sm text-gray-500">${item.price}</p>
+                          </div>
+                          <p className="mt-1 text-sm text-gray-500">Qty: {item.quantity}</p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                )}
               </div>
-            </section>
+              <div className="mt-6 flex-shrink-0 px-4 py-4 flex justify-end border-t border-gray-200">
+                <p className="text-lg text-gray-900">${cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0)}</p>
+              </div>
+            </div>
           </div>
-        </div>
-      )}
-    </>
-  );
+        </section>
+      </div>
+    </div>
+  )}
+</>
+);
 };
 
 export default CartDrawer;
--- src/pages/Showp.tsx
+++ src/pages/Showp.tsx
@@ -1,39 +1,47 @@
-import products from '../components/info'
+import product  from "../components/info";
 
 interface ProductInfo {
   image: string
   description: string
   price: number
 }
-
 interface CardProps {
-  product: ProductInfo
+  product: ProductInfo;
+  handleAdd: (product: ProductInfo) => void;
 }
 
-const Card: React.FC<CardProps> = ({ product }) => {
-  const { image, description, price } = product
+const Card: React.FC<CardProps> = ({ product, handleAdd }) => {
+  const { image, description, price } = product;
+
+  const handleClick = () => {
+    handleAdd(product);
+  };
+
   return (
     <div className="rounded-lg overflow-hidden shadow-lg bg-white">
       <img className="w-full" src={image} alt="Product" />
       <div className="p-4">
         <p className="mt-2 text-gray-600">{description}</p>
         <p className="mt-2 text-gray-800 font-medium">${price.toFixed(2)}</p>
-        <button className="mt-4 block w-full rounded-lg py-2 px-3 bg-blue-500 text-white font-medium hover:bg-blue-600">
+        <button
+          className="mt-4 block w-full rounded-lg py-2 px-3 bg-blue-500 text-white font-medium hover:bg-blue-600"
+          onClick={handleClick}
+        >
           Add to Cart
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-const CardGrid = () => {
+const CardGrid: React.FC<{ products: ProductInfo[]; handleAdd: (product: ProductInfo) => void }> = ({ products, handleAdd }) => {
   return (
     <div className="grid gap-3 grid-cols-3 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3">
       {products.map((product, index) => (
-        <Card key={index} product={product} />
+        <Card key={index} product={product} handleAdd={handleAdd} />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default CardGrid
+export default CardGrid;
