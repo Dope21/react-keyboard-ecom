ed3525960c9cac211c8728ede751e37de9e9d7b2
ten14301 <ten077710@gmail.com>
2023-04-12 00:39:37 +0700

    add product

--- src/pages/Showp.tsx
+++ src/pages/Showp.tsx
@@ -1,20 +1,33 @@
-import product  from "../components/info";
+import { useState } from 'react';
+import products from '../components/info';
 
 interface ProductInfo {
-  image: string
-  description: string
-  price: number
+  image: string;
+  description: string;
+  price: number;
 }
+
 interface CardProps {
   product: ProductInfo;
-  handleAdd: (product: ProductInfo) => void;
+  onAddToCart: (product: ProductInfo, quantity: number) => void;
 }
 
-const Card: React.FC<CardProps> = ({ product, handleAdd }) => {
+const Card: React.FC<CardProps> = ({ product, onAddToCart }) => {
   const { image, description, price } = product;
+  const [quantity, setQuantity] = useState(1);
 
-  const handleClick = () => {
-    handleAdd(product);
+  const handleAddToCart = () => {
+    onAddToCart(product, quantity);
+  };
+
+  const handleAdd = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const handleSubtract = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
   };
 
   return (
@@ -23,22 +36,50 @@ const Card: React.FC<CardProps> = ({ product, handleAdd }) => {
       <div className="p-4">
         <p className="mt-2 text-gray-600">{description}</p>
         <p className="mt-2 text-gray-800 font-medium">${price.toFixed(2)}</p>
-        <button
-          className="mt-4 block w-full rounded-lg py-2 px-3 bg-blue-500 text-white font-medium hover:bg-blue-600"
-          onClick={handleClick}
-        >
-          Add to Cart
-        </button>
+        <div className="flex items-center mt-4">
+          <button
+            className="text-gray-500 hover:text-gray-700 focus:outline-none"
+            onClick={handleSubtract}
+          >
+            -
+          </button>
+          <span className="mx-2 text-gray-700">{quantity}</span>
+          <button
+            className="text-gray-500 hover:text-gray-700 focus:outline-none"
+            onClick={handleAdd}
+          >
+            +
+          </button>
+          <button
+            className="ml-auto block rounded-lg py-2 px-3 bg-blue-500 text-white font-medium hover:bg-blue-600"
+            onClick={handleAddToCart}
+          >
+            Add to Cart
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
-const CardGrid: React.FC<{ products: ProductInfo[]; handleAdd: (product: ProductInfo) => void }> = ({ products, handleAdd }) => {
+const CardGrid = () => {
+  const [cart, setCart] = useState<{ product: ProductInfo; quantity: number }[]>([]);
+
+  const handleAddToCart = (product: ProductInfo, quantity: number) => {
+    const newCart = [...cart];
+    const itemIndex = newCart.findIndex((item) => item.product === product);
+    if (itemIndex === -1) {
+      newCart.push({ product, quantity });
+    } else {
+      newCart[itemIndex].quantity += quantity;
+    }
+    setCart(newCart);
+  };
+
   return (
     <div className="grid gap-3 grid-cols-3 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3">
       {products.map((product, index) => (
-        <Card key={index} product={product} handleAdd={handleAdd} />
+        <Card key={index} product={product} onAddToCart={handleAddToCart} />
       ))}
     </div>
   );
