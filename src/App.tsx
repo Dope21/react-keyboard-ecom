1bba04d7468d58952c28758ce04fbb8ac90ac278
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-04-11 17:29:32 +0700

    finish home page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Container from './components/Container'
 import Login from './pages/Login'
 import CreateAccount from './pages/CreateAccount'
+import Home from './pages/Home'
 import './styles/index.css'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path="/login" Component={Login} />
             <Route path="/register" Component={CreateAccount} />
+            <Route path="/" Component={Home} />
           </Routes>
         </Router>
       </Container>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,39 @@
+import Container from '../components/Container'
+import { HiArrowLongRight } from 'react-icons/hi2'
+
+const Home = () => {
+  return (
+    <div>
+      <section className="absolute left-0 top-12 z-10">
+        <div>
+          <img
+            src="https://cdn.shopify.com/s/files/1/0059/0630/1017/files/K1-PRO--1_1944x.jpg?v=1678929896"
+            alt="banner"
+          />
+        </div>
+        <div className="mt-[-11rem] md:mt-[-19rem]">
+          <Container>
+            <div>
+              <h2 className="text-3xl md:text-5xl font-bold text-white mb:mb-4">
+                Keychron K1 Pro
+              </h2>
+            </div>
+            <div className="flex gap-6 items-center">
+              <p className="text-lg md:text-2xl font-semibold text-white">
+                QMK | LOW | PROFILE | 80% LAYOUT
+              </p>
+              <div className="group text-base md:text-lg text-white border-2 border-white px-5 py-3 font-bold tracking-wider transition-all flex items-center gap-4 hover:text-black hover:bg-white ">
+                <a href="/">Shop Now</a>
+                <div className="group-hover:inline-block group-hover:opacity-100 group-hover:ml-0 opacity-0 transition-all duration-500 ml-[-2rem]">
+                  <HiArrowLongRight />
+                </div>
+              </div>
+            </div>
+          </Container>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Home
