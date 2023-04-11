b5e0a82a046a77c2351afa686982cc9187528abd
MGEternal <gmgame1020@outlook.com>
2023-04-11 18:01:13 +0700

    update

--- src/components/Showp.tsx
+++ src/components/Showp.tsx
@@ -1,4 +1,7 @@
+import React from 'react';
+
 interface ProductInfo {
+    
     image: string;
     description: string;
     price: number;
@@ -7,7 +10,7 @@ interface ProductInfo {
   interface CardProps {
     product: ProductInfo;
   }
-  
+
   const Card: React.FC<CardProps> = ({ product }) => {
     const { image, description, price } = product;
     return (
@@ -16,9 +19,11 @@ interface ProductInfo {
         <div className="p-4">
           <p className="mt-2 text-gray-600">{description}</p>
           <p className="mt-2 text-gray-800 font-medium">${price.toFixed(2)}</p>
+          
           <button className="mt-4 block w-full rounded-lg py-2 px-3 bg-blue-500 text-white font-medium hover:bg-blue-600">
-            Add to Cart
+            Detail
           </button>
+          
         </div>
       </div>
     );
@@ -33,6 +38,6 @@ interface ProductInfo {
       </div>
     );
   };
-  
+ 
   export default CardGrid;
   
