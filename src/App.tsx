9ef40a76fdac6f32358066ab5d98a2ef9be2072b
MGEternal <gmgame1020@outlook.com>
2023-04-11 17:41:30 +0700

    move info

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,9 @@
 import Navbar from './components/Navbar';
 import './styles/index.css';
 import CardGrid from './components/Showp';
+import products from './components/info';
 const App: React.FC = () => {
-  const products = [
-    {
-      title: 'Product 1',
-      image: 'https://source.unsplash.com/random/400x200',
-      description: 'Description of product 1',
-      price: 12.99,
-    },
-    {
-      title: 'Product 2',
-      image: 'https://source.unsplash.com/random/400x200',
-      description: 'Description of product 2',
-      price: 19.99,
-    },
-    {
-      title: 'Product 3',
-      image: 'https://source.unsplash.com/random/400x200',
-      description: 'Description of product 3',
-      price: 7.99,
-    },
-    {
-      title: 'Product 4',
-      image: 'https://source.unsplash.com/random/400x200',
-      description: 'Description of product 3',
-      price: 11.92,
-    },
-    {
-      title: 'Product 5',
-      image: 'https://source.unsplash.com/random/400x200',
-      description: 'Description of product 3',
-      price: 11.92,
-    }
-  ];
+  
 
   return (
     <>
