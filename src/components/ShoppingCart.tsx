afee947f5beb3b28a4505f92e4edeb17fe94e0d5
ten14301 <ten077710@gmail.com>
2023-04-13 11:23:04 +0700

    cartwork

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,20 +1,39 @@
-import { NavItem, Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "./ShoppingCartContext";
 
 type ShoppingCartProps = {
-    isOpen: boolean
-}
+  isOpen: boolean;
+};
 
-export function ShoppingCart({isOpen}:ShoppingCartProps){
-    const {closecart, cartItems } = useShoppingCart()
-    return (
-        <Offcanvas open={true} onHide={closecart} placement="end">
-            <Offcanvas.Header closeButton>
-                <Offcanvas.Title>Cart</Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
+export function ShoppingCart({ isOpen }: ShoppingCartProps) {
+  const { closecart } = useShoppingCart();
 
-            </Offcanvas.Body>
-        </Offcanvas>
-    )
+  return (
+    <>
+      {isOpen && (
+        <div className="fixed top-0 right-0 h-screen w-full">
+          <div className="absolute top-0 right-0 h-screen w-full z-40"
+            onClick={closecart}
+          >
+            <div className="bg-black opacity-50 w-full h-full"></div>
+          </div>
+          <div className="fixed top-0 right-0 h-screen w-full flex items-center justify-center z-50">
+            <div className="bg-white rounded-lg w-96">
+              <div className="p-6">
+                <h2 className="text-lg font-medium mb-4">Your cart</h2>
+                {/* Render your cart items here */}
+              </div>
+              <div className="p-6 flex justify-end">
+                <button
+                  className="text-white bg-red-500 hover:bg-red-600 py-2 px-4 rounded"
+                  onClick={closecart}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
