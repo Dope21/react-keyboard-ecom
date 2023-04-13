b450b6006e89ca68dbd9d10f81bcf45aec658aee
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-04-13 10:08:38 +0700

    add more margin top

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Container = ({ children }: Props) => {
   return (
-    <main className="mt-16 md:mt-28">
+    <main className="mt-24 md:mt-36">
       <div className="px-4 md:px-10 mx-auto max-w-7xl">{children}</div>
     </main>
   )
