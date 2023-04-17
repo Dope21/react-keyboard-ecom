46ccbd2576607bd6f5f9a308c479ad309c9d896a
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-04-17 20:53:14 +0700

    merge main

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,10 @@ import Login from './pages/Login'
 import CreateAccount from './pages/CreateAccount'
 import Home from './pages/Home'
 import './styles/index.css'
-<<<<<<< HEAD
-import CardGrid from './pages/Showp'
 import Checkout from './pages/Checkout'
-
-=======
 import { Products } from './pages/Showp'
 import { ShoppingCartProvider } from './components/ShoppingCartContext'
 import ProductDetail from './pages/ProductDetail'
->>>>>>> 698d30bf9aa427dec90084d1ffebc7b99667298b
 function App() {
   return (
     <ShoppingCartProvider>
@@ -25,14 +20,9 @@ function App() {
             <Route path="/login" Component={Login} />
             <Route path="/register" Component={CreateAccount} />
             <Route path="/" Component={Home} />
-<<<<<<< HEAD
-            <Route path="/products" Component={CardGrid} />
             <Route path="/checkout" Component={Checkout} />
-=======
             <Route path="/products" Component={Products} />
-            
             <Route path="/products/:id" Component={ProductDetail} />
->>>>>>> 698d30bf9aa427dec90084d1ffebc7b99667298b
           </Routes>
         </Router>
       </Container>
