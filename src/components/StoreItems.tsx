34f22b703df7937a65fbe37ef3ae46513c55d0d7
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-05-02 21:51:05 +0700

    fetch data with axios

--- src/components/StoreItems.tsx
+++ src/components/StoreItems.tsx
@@ -1,45 +1,68 @@
-import { Button, Card } from "react-bootstrap";
-import { formatCurrency } from "./formatCurrency";
-import { useShoppingCart } from "./ShoppingCartContext";
-import { Link } from 'react-router-dom';
+import { Button, Card } from 'react-bootstrap'
+import { formatCurrency } from './formatCurrency'
+import { useShoppingCart } from './ShoppingCartContext'
+import { Link } from 'react-router-dom'
 
 type StoreItemProps = {
-  "id":number,
-  "title": string,
-  "image": string,
-  "description": string,
-  "price": number,
+  id: number
+  name: string
+  price: number
+  description: string
+  image: string
+  qty: number
+  version: string
+  type: string
+  switches: string
+  color: string
+  category: string
 }
 
-export function StoreItems({id,title, image, description, price}: StoreItemProps) {
-  const { getItemsQuantity, increaseQuantity,removeQuantity} = useShoppingCart()
+export function StoreItems({
+  id,
+  name,
+  image,
+  description,
+  price,
+  category
+}: StoreItemProps) {
+  const { getItemsQuantity, increaseQuantity, removeQuantity } =
+    useShoppingCart()
   const quantity = 0
   return (
-    
     <Card className="border border-gray-300 rounded-lg overflow-hidden p-4">
       <div className="relative h-48 overflow-hidden">
-        <Card.Img variant="top" src={image} alt={title} className="object-cover w-full h-full" />
+        <Card.Img
+          variant="top"
+          src={image}
+          alt={name}
+          className="object-cover w-full h-full"
+        />
       </div>
       <Card.Body>
         <div className="flex justify-between items-center mb-2">
-          <h2 className="text-lg font-medium text-gray-800">{title}</h2>
-          <span className="text-gray-800 font-medium">{formatCurrency(price)}</span>
+          <h2 className="text-lg font-medium text-gray-800">{name}</h2>
+          <span className="text-gray-800 font-medium">
+            {formatCurrency(price)}
+          </span>
         </div>
-        <p className="text-sm text-gray-600 mb-2">{description}</p>
+        <p className="text-sm text-gray-600 mb-2 w-80 truncate">
+          {description}
+        </p>
 
         <div className="flex justify-center flex-col">
-  <Button className="w-full bg-green-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded mb-2"  onClick={()=>increaseQuantity(id)}>
-    Add To Cart
-  </Button>
-  <Link to={`/products/${id}`} className="mt-2">
-    <Button className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded mb-2">
-      Detail
-    </Button>
-  </Link>
-</div>
-
-
+          <Button
+            className="w-full bg-green-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded mb-2"
+            onClick={() => increaseQuantity(id)}
+          >
+            Add To Cart
+          </Button>
+          <Link to={`/products/${category}/${id}`} className="mt-2">
+            <Button className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded mb-2">
+              Detail
+            </Button>
+          </Link>
+        </div>
       </Card.Body>
     </Card>
-  );
+  )
 }
